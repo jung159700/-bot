046391b8b9b4e65e1ca4f7a1ac4513e166c0bf39
jung159700 <81882420+jung159700@users.noreply.github.com>
2021-04-04 07:55:44 +0900

    Update dicobot.py

--- dicobot.py
+++ dicobot.py
@@ -4,12 +4,6 @@ import os
 
 client = discord.Client()
 
-@client.event
-async def on_ready():
-    print(client.user.id)
-    print("ready")
-    game = discord.Game("나 대신 있는 봇")
-    await client.change_presence(status=discord.Status.online, activity=game)
 
 @client.event
 async def on_message(meassage):
